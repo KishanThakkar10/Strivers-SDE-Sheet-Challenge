61b5ab68852fa7ad969884be63d007cd97814468
Kishan Thakkar <61037613+KishanThakkar10@users.noreply.github.com>
2023-06-10 17:42:31 +0530

    10 June 2023

--- Day_4_Arrays/pair_sum.cpp
+++ Day_4_Arrays/pair_sum.cpp
@@ -1,1 +1,13 @@
+#include <bits/stdc++.h>
 
+vector<vector<int>> pairSum(vector<int> &arr, int s){
+   vector<vector<int>> ans;
+   int n = arr.size();
+   sort(arr.begin(),arr.end());
+   for(int i=0; i<n-1 ;i++){
+      for(int j = i+1; j<n ; j++){
+         if((arr[i]+arr[j]) == s) ans.push_back({arr[i],arr[j]});
+      }
+   }
+   return ans;
+}
